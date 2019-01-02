6fa6530aeb3059f250fc0b32b90ed3c20c898737
JieanYang <jiean.yang.24@gmail.com>
2019-01-02 22:42:05 +0100

    Function Components;

--- src/index.js
+++ src/index.js
@@ -3,17 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button 
-        className="square" 
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+	return(
+		<button className="square" onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+	);
 }
 
 class Board extends React.Component {
@@ -33,7 +28,7 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
     	<Square 
-    		value={this.state.squares[i]} 
+    		value = {this.state.squares[i]} 
     		onClick = {() => this.handleClick(i)}
     	/>
     );
