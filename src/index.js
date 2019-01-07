efa011d604a0c60ff7c01ffd7ebec03ae21d59df
JieanYang <jiean.yang.24@gmail.com>
2019-01-07 22:35:50 +0100

    Lifting State Up -> temperature calculator for boiling verdict

--- src/index.js
+++ src/index.js
@@ -1,150 +1,99 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 
+const scaleNames = {
+  c: 'Celsius',
+  f: 'Fahrenheit'
+};
 
-function Square(props) {
-	return(
-		<button className="square" onClick={() => props.onClick()}>
-      {props.value}
-    </button>
-	);
+function toCelsius(fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9;
 }
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return (
-    	<Square 
-    		value = {this.props.squares[i]} 
-    		onClick = {() => this.props.onClick(i)}
-    	/>
-    );
+function toFahrenheit(celsius) {
+  return (celsius * 9 / 5) + 32;
+}
+
+function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return '';
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>The water would boil.</p>;
+  }
+  return <p>The water would not boil.</p>;
+}
+
+class TemperatureInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.props.onTemperatureChange(e.target.value);
   }
 
   render() {
+    const temperature = this.props.temperature;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}:</legend>
+        <input value={temperature}
+               onChange={this.handleChange} />
+      </fieldset>
+    );
+  }
+}
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+    this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+    this.state = {temperature: '', scale: 'c'};
+  }
+
+  handleCelsiusChange(temperature) {
+    this.setState({scale: 'c', temperature});
+  }
+
+  handleFahrenheitChange(temperature) {
+    this.setState({scale: 'f', temperature});
+  }
+
+  render() {
+    const scale = this.state.scale;
+    const temperature = this.state.temperature;
+    const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+    const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        <TemperatureInput
+          scale="c"
+          temperature={celsius}
+          onTemperatureChange={this.handleCelsiusChange} />
+        <TemperatureInput
+          scale="f"
+          temperature={fahrenheit}
+          onTemperatureChange={this.handleFahrenheitChange} />
+        <BoilingVerdict
+          celsius={parseFloat(celsius)} />
       </div>
     );
   }
 }
-
-class Game extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			history: [{
-				squares: Array(9).fill(null),
-			}],
-			stepNumber: 0,
-			xIsNext: true,
-		};
-	}
-
-	handleClick(i) {
-		const history = this.state.history.slice(0, this.state.stepNumber + 1);
-		const current = history[history.length - 1];
-		const squares = current.squares.slice();
-		if(calculateWinner(squares) || squares[i]){
-			return;
-		}
-		squares[i] = this.state.xIsNext? 'X' : 'O';
-		this.setState({
-			history: history.concat([{
-				squares: squares,
-			}]),
-			stepNumber: history.length,
-			xIsNext: !this.state.xIsNext,
-		});
-	}
-
-	jumpTo(step) {
-		this.setState({
-			stepNumber: step,
-			xIsNext: (step % 2) === 0,
-		});
-	}
-
-  render() {
-  	const history = this.state.history;
-  	const current = history[this.state.stepNumber];
-  	const winner = calculateWinner(current.squares);
-
-  	const moves = history.map((step, move) => {
-  		const desc = move ? 
-  			'Go to move #' + move : 
-  			'Go to game start';
-  		return(
-  			<li key = {move}>
-  				<button onClick = {() => this.jumpTo(move)}>{desc}</button>
-  			</li>
-  		);
-  	});
-
-  	let status;
-  	if(winner) {
-  		status = 'Winner: ' + winner;
-  	} else {
-  		status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-  	}
-
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board 
-          	squares = {current.squares}
-          	onClick = {(i) => this.handleClick(i)}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-function calculateWinner(squares) {
-	const lines = [
-		[0,1,2],
-		[3,4,5],
-		[6,7,8],
-		[0,3,6],
-		[1,4,7],
-		[2,5,8],
-		[0,4,8],
-		[2,4,6],
-	];
-	for(let i = 0; i < lines.length; i++) {
-		const [a,b,c] = lines[i];
-		if(squares[a] && squares[a] === squares[b] && 
-			squares[a] === squares[c]) {
-			return squares[a];
-		}
-	}
-
-	return null;
-}
-
-// ========================================
 
 ReactDOM.render(
-  <Game />,
+  <Calculator />,
   document.getElementById('root')
 );
