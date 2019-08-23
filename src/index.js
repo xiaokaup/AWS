27b344850573daf9d69002e542735d4ebf9ac2f0
Jiean Yang <jiean.yang8534@gmail.com>
2019-08-23 16:21:02 +0200

    Initiate store of redux

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import React from "react";
-import { render } from "react-dom";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import rootReducer from "./reducers"
-import App from "../components/App";
-// import "./index.css";
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
+import App from './components/App'
 
 const store = createStore(rootReducer)
 
 render(
-  <Provider store = {store}>
-  	<App />
-  </Provider>, 
-  document.getElementById("root")
-  )
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
