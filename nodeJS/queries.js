a05a946c639a424cce281f085642bc04d1b73100
Jiean Yang <jiean.yang8534@gmail.com>
2019-10-06 20:13:26 +0200

    Complete APIs

--- nodeJS/queries.js
+++ nodeJS/queries.js
@@ -23,7 +23,42 @@ const getUserById = (request, response) => {
 	})
 }
 
+const createUser = (request, response) => {
+	const {name, email} = request.body;
+
+	pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+		if (error) throw error;
+		response.status(201).send(`User added with ID: ${results.insertId}`);
+	});
+}
+
+const updateUser = (request, response) => {
+	const id = parseInt(request.params.id);
+	const {name, email} = request.body;
+
+	pool.query(
+		'UPDATE users SET name = $1, email = $2 WHERE id =$3',
+		[name, email, id],
+		(error, results) => {
+			if (error) throw error;
+			response.status(200).send(`User modified with ID: ${id}`);
+		}
+	);
+}
+
+const deleteUser = (request, response) => {
+	const id = parseInt(request.params.id);
+
+	pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+		if (error) throw error;
+		response.status(200).send(`User deleted with ID: ${id}`);
+	});
+}
+
 module.exports = {
 	getUsers,
-	getUserById
+	getUserById,
+	createUser,
+	updateUser,
+	deleteUser
 }
--- nodeJS/server.js
+++ nodeJS/server.js
@@ -21,6 +21,9 @@ app.get('/', (request, response) => {
 
 app.get('/users', db.getUsers);
 app.get('/users/:id', db.getUserById);
+app.post('/users', db.createUser);
+app.put('/users/:id', db.updateUser);
+app.delete('/users/:id', db.deleteUser);
 
 app.listen(PORT, () => {
 	console.log(`App running on port ${PORT}`)
