fa20d4c94c277889c8e111a49295f5fc23909ae2
Jiean Yang <jiean.yang8534@gmail.com>
2019-10-09 12:21:31 +0200

    Update User module

--- flask/src/model.py
+++ flask/src/model.py
@@ -5,8 +5,8 @@ class User(db.Model):
 
 	id = db.Column(db.Integer, primary_key=True)
 	email = db.Column(db.String(64), unique=True, index=True)
-	username = db.Column(db.String(64), unique=True, index=True)
-	password_hash = db.Column(db.String(128))
+	# username = db.Column(db.String(64), unique=True, index=True)
+	# password_hash = db.Column(db.String(128))
 	name = db.Column(db.String(64))
 	
 	def __repr__(slef):
