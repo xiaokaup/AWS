a8e0281a45d8d85247590ef7f36e8247ad7a7243
Jiean Yang <jiean.yang8534@gmail.com>
2019-10-09 12:27:59 +0200

    Change link for postgres

--- flask/src/config.py
+++ flask/src/config.py
@@ -2,8 +2,7 @@ import os
 from app import app
 from flask_sqlalchemy import SQLAlchemy
 
-SALQLCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL') or \
-"postgresql://postgres:postgres@postgres:5432/db"
+SQLALCHEMY_DATABASE_URI = "postgresql://me:me@postgres:5432/db"
 
 
 app.config.from_object(__name__)
