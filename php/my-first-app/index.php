e5fd6a2fa016606bd49e9f354bb7a58f56f62120
Jiean Yang <jiean.yang8534@gmail.com>
2019-10-10 15:04:03 +0200

    Global and local variables

--- php/my-first-app/index.php
+++ php/my-first-app/index.php
@@ -10,6 +10,33 @@ echo '<br>';
 echo 'my-first-app';
 ?>
 
+<!-- global and local variables -->
+<?php
+$x=5;
+$z=20;
+$a=30;
+
+function myTest() {
+	$y=10;
+	global $z;
+	echo "<p>测试函数内变量:</p>";
+	echo "x=$x";
+	echo "<br>";
+	echo "y=$y";
+	echo "<br>";
+	echo "z=$z";
+	echo "<br>";
+	echo "a=".$GLOBALS['a'];
+}
+
+myTest();
+
+echo "<p>测试函数外变量:</p>";
+echo "x=$x";
+echo "<br>";
+echo "y=$y";
+?>
+
 </body>
 </html>
 
