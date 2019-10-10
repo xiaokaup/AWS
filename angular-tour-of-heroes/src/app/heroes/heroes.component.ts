90e3be5b1901d4f7a416f21c3f40823452b4abe4
Jiean Yang <jiean.yang8534@gmail.com>
2019-10-10 23:42:06 +0200

    5. Routing

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -17,11 +17,6 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-  	this.selectedHero = hero;
-  }
-
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
