c0e2355dde835b4e287b53f60eb246b35cd0a850
Jiean Yang <jiean.yang8534@gmail.com>
2019-10-10 11:59:54 +0200

    4. Services--HeroService

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,4 +1,4 @@
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
@@ -9,15 +9,21 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[];
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   selectedHero: Hero;
   onSelect(hero: Hero): void {
   	this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 }
