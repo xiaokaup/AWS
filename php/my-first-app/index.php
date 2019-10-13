063119a3979725ea555d3ffbe7dcc36b36d1dc2c
Jiean Yang <jiean.yang8534@gmail.com>
2019-10-13 10:26:58 +0200

    PHP Switch

--- php/my-first-app/index.php
+++ php/my-first-app/index.php
@@ -12,17 +12,38 @@ echo '<br>';
 echo '<br>';
 ?>
 
+<!-- PHP Switch -->
 <?php
-$t=date('H');
-if ($t<"10") {
-	echo "Have a good morning!";
-} else if ($t<"20") {
-	echo "Have a good day!";
-} else {
-	echo "Have a good night!";
+$favcolor="red";
+switch($favcolor)
+{
+	case "red":
+			echo "你喜欢的颜色是红色！";
+			break;
+	case "blue":
+			echo "你喜欢的颜色是蓝色！";
+			break;
+	case "green":
+			echo "你喜欢的颜色是绿色！";
+			break;
+	default:
+			echo "你喜欢的颜色不是 红，蓝，或绿色！";
 }
 ?>
 
+<!-- PHP if...Else -->
+<?php
+// $t=date('H');
+// if ($t<"10") {
+// 	echo "Have a good morning!";
+// } else if ($t<"20") {
+// 	echo "Have a good day!";
+// } else {
+// 	echo "Have a good night!";
+// }
+?>
+
+<!-- PHP constant -->
 <?php
 // // 区分大小写的常量名
 // define("GREETING", "欢迎访问 Runoob.com");
