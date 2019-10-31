664d16339df7c6baba19b7a91691a830ab753afa
Jiean Yang <jiean.yang8534@gmail.com>
2019-10-31 16:13:56 +0100

    Simplify code

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -3,19 +3,21 @@ import { connect } from 'react-redux'
 import { addTodo } from '../actions'
 
 const AddTodo = ({ dispatch }) => {
-  let input
+  let input;
+
+  const submitForm = e => {
+    e.preventDefault();
+    if (!input.value.trim()) {
+      return;
+    }
+    dispatch(addTodo(input.value));
+    input.value = '';
+  };
 
   return (
     <div>
       <form
-        onSubmit={e => {
-          e.preventDefault()
-          if (!input.value.trim()) {
-            return
-          }
-          dispatch(addTodo(input.value))
-          input.value = ''
-        }}
+        onSubmit={submitForm}
       >
         <input ref={node => (input = node)} />
         <button type="submit">Add Todo</button>
