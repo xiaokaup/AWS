eb06cf4bd77aa66a57f64baef37fe3d41dda644d
Jiean Yang <jiean.yang8534@gmail.com>
2019-11-20 16:07:54 +0100

    Optimize codes

--- react/src/App.js
+++ react/src/App.js
@@ -1,19 +1,24 @@
-import React from 'react'
-// import Header from './pages/Common/Header'
-// import NavBar from './pages/Common/NavBar'
-// import Footer from './pages/Common/Footer'
+
+import React, { Fragment } from 'react'
+import Header from './pages/Common/Header'
+import NavBar from './pages/Common/NavBar'
+import Footer from './pages/Common/Footer'
 import { Route } from 'react-router-dom'
+import HomePage from './pages/HomePage/HomePage'
 import './App.css'
 
-const AppSinglePageRoute = ({ component: Component, ...rest }) => (
-<Route
-  {...rest}
-  render={routeProps => (
-  	<div>
-		<Component {...routeProps} />		
-	</div>
-  )}
-/>
+
+
+const App = ({ component: Component, ...rest }) => (
+	<Fragment>
+		<Header />
+		<NavBar />
+	  		<switch>
+			    <Route exact path="/" component={() => <div><h1 style={{color:"black"}}>Home</h1></div>} />
+	  			<Route path="/app" component={HomePage} />
+			</switch>
+	  	<Footer />
+  	</Fragment>
 )
 
-export default AppSinglePageRoute
+export default App
