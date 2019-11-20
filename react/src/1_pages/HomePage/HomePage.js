53de2d293845cf94f29a74ef2bb6ffe1457e9944
Jiean Yang <jiean.yang8534@gmail.com>
2019-11-20 17:39:11 +0100

    Transfer HomePage from function to class

--- react/src/1_pages/HomePage/HomePage.js
+++ react/src/1_pages/HomePage/HomePage.js
@@ -3,21 +3,40 @@ import './HomePage.css'
 import { Cards } from '../../3_elements/Cards'
 
 
-const HomePage = () => (
-	<Fragment>
-		<div id="messages">
-			{/*<app-messages></app-messages>*/}
-		</div>
-		<div id="content">
-			{/*<router-outlet></router-outlet>*/}
-			<h3>Top Heroes</h3>
-			<div class="grid grid-pad">
-				<Cards cards={[{id:1, name:"a"}, {id:2, name:"b"}, {id:3, name:"c"}, {id:4, name:"d"}]} />
-			</div>
+class HomePage extends React.Component  {
+	constructor(props) {
+		super(props)
+		this.state = {cards: [{id:1, name:"a"}, {id:2, name:"b"}, {id:3, name:"c"}, {id:4, name:"d"}]}
+	}
 
-			{/*<app-hero-search></app-hero-search>*/}
-		</div>
-	</Fragment>
-)
+	// componentDidMount() {
+	// 	fetch("http://35.180.32.33/nodejs/heroes")
+	// 		.then(response => {
+	// 			return response.json();
+	// 		})
+	// 		.then(heroes => {
+	// 			console.log(heroes);
+	// 		})
+	// }
+
+	render() {
+		return (
+			<Fragment>
+				<div id="messages">
+					{/*<app-messages></app-messages>*/}
+				</div>
+				<div id="content">
+					{/*<router-outlet></router-outlet>*/}
+					<h3>Top Heroes</h3>
+					<div class="grid grid-pad">
+						<Cards cards={this.state.cards} />
+					</div>
+
+					{/*<app-hero-search></app-hero-search>*/}
+				</div>
+			</Fragment>
+		);
+	}
+}
 
 export default HomePage
