ada677f8b09eb13e4f2f7dac3d350675c66905f1
Jiean Yang <jiean.yang8534@gmail.com>
2019-11-22 10:22:24 +0100

    Add method deleteHero

--- react/src/1_pages/Heroes/Heroes.js
+++ react/src/1_pages/Heroes/Heroes.js
@@ -9,7 +9,8 @@ class Heroes extends React.Component  {
 		this.state = {lists: [], newHeroName: ""}
 
 		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleAddHero = this.handleAddHero.bind(this);
+		this.handleDeleteHero = this.handleDeleteHero.bind(this);
 	}
 
 	componentDidMount() {
@@ -26,9 +27,7 @@ class Heroes extends React.Component  {
 		this.setState({newHeroName: event.target.value});
 	}
 
-	handleSubmit(event) {
-		// alert(this.state.newHeroName);
-		
+	handleAddHero(event) {
 		const postHero = (url, data) => {
 		  // Default options are marked with *
 		  return fetch(url, {
@@ -56,6 +55,35 @@ class Heroes extends React.Component  {
 			.catch(error => console.log(error))
 	}
 
+	handleDeleteHero(event, id) {
+		const deleteHero = (url) => {
+		  // Default options are marked with *
+		  return fetch(url, {
+		    headers: {
+		      // 'user-agent': 'Mozilla/4.0 MDN Example',
+		      'content-type': 'application/json'
+		    },
+		    method: 'DELETE', // *GET, POST, PUT, DELETE, etc.
+		    // body: JSON.stringify(data), // must match 'Content-Type' header
+		    // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+		    // credentials: 'same-origin', // include, same-origin, *omit
+		    // mode: 'cors', // no-cors, cors, *same-origin
+		    // redirect: 'follow', // manual, *follow, error
+		    // referrer: 'no-referrer', // *client, no-referrer
+		  })
+		  .then(response => response.json()) // parses response to JSON
+		}
+
+		deleteHero("http://35.180.32.33/nodejs/heroes/"+id)
+			.then(data => {
+				const heroes = this.state.lists;
+				const heroes_filter = heroes.filter(x => {return x.id !== data.id})
+				this.setState({lists: heroes_filter});
+			})
+			.catch(error => console.log(error))
+
+	}
+
 	render() {
 		return (
 			<Fragment>
@@ -68,12 +96,12 @@ class Heroes extends React.Component  {
 						<label>Hero name:
 							<input type="text" value={this.state.newHeroName} onChange={this.handleChange} />
 						</label>
-						<button onClick={this.handleSubmit}>
+						<button onClick={this.handleAddHero}>
 							add
 						</button>
 					</div>
 					
-					<Lists ul_class="heroes" specific_style={null} lists={this.state.lists} />
+					<Lists ul_class="heroes" specific_style={null} lists={this.state.lists} handleDeleteHero={this.handleDeleteHero} />
 				
 					{/*<app-hero-search></app-hero-search>*/}
 				</div>
--- react/src/3_elements/Lists.js
+++ react/src/3_elements/Lists.js
@@ -2,11 +2,11 @@ import React, {Fragment} from 'react'
 
 export function Lists(props) {
 	const Lists = props.lists.map(list => 
-				<li style={props.specific_style}>
+				<li style={props.specific_style} key={list.id}>
 					<a href={"/detail/"+list.id} style={props.specific_style}>
 						<span class="badge" style={props.specific_style}>{list.id}</span> {list.name}
 					</a>
-					<button class="delete" title="delete hero" style={props.specific_style}>x</button>
+					<button class="delete" title="delete hero" style={props.specific_style} onClick={event => props.handleDeleteHero(event, list.id)}>x</button>
 				</li>
 			);
 
