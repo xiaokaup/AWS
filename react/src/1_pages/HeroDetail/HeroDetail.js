72e25c25e2795bf0e7733bb33fa25322b5dbb2c9
Jiean Yang <jiean.yang8534@gmail.com>
2019-11-22 12:38:00 +0100

    Add function updateHero

--- react/src/1_pages/HeroDetail/HeroDetail.js
+++ react/src/1_pages/HeroDetail/HeroDetail.js
@@ -5,9 +5,14 @@ import './HeroDetail.css';
 class HeroDetail extends React.Component  {
 	constructor(props) {
 		super(props)
-		this.state = {hero: {id: 1, name: 'jiean'}}
+		this.state = {
+			hero_id: 0,
+			hero_name: '',
+			hero_update_name: ''
+		}
 
 		this.handleChange = this.handleChange.bind(this);
+		this.save = this.save.bind(this);
 		this.goBack = this.goBack.bind(this);
 	}
 
@@ -18,16 +23,43 @@ class HeroDetail extends React.Component  {
 				return response.json();
 			})
 			.then(hero => {
-				this.setState({hero: hero});
+				this.setState({hero_name: hero.name, hero_id: hero.id});
 			})
 	}
 
 	handleChange(event) {
-		this.setState({hero: {name: event.target.value}});
+		this.setState({hero_update_name: event.target.value});
 	}
 
 	goBack() {
 		this.props.history.goBack();
+	}
+
+	save() {
+		const putHero = (url, data) => {
+		  // Default options are marked with *
+		  return fetch(url, {
+		    headers: {
+		      // 'user-agent': 'Mozilla/4.0 MDN Example',
+		      'content-type': 'application/json'
+		    },
+		    method: 'PUT', // *GET, POST, PUT, DELETE, etc.
+		    body: JSON.stringify(data), // must match 'Content-Type' header
+		    // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+		    // credentials: 'same-origin', // include, same-origin, *omit
+		    // mode: 'cors', // no-cors, cors, *same-origin
+		    // redirect: 'follow', // manual, *follow, error
+		    // referrer: 'no-referrer', // *client, no-referrer
+		  })
+		  .then(response => response.json()) // parses response to JSON
+		}
+
+		const id = this.state.hero_id;
+		putHero("http://35.180.32.33/nodejs/heroes/"+id, {name: this.state.hero_update_name, id: id})
+			.then(hero_update => {
+				this.setState({hero_name: hero_update.name})
+			})
+			.catch(error => console.log(error))
 	}
 
 	render() {
@@ -38,18 +70,18 @@ class HeroDetail extends React.Component  {
 				</div>
 				<div id="content">
 				{
-					this.state.hero
+					this.state.hero_id && this.state.hero_name
 					&&
 					<Fragment>
-						<h2>{this.state.hero.name.toUpperCase()} Details</h2>
-						<div><span>id: </span>{this.state.hero.id}</div>
+						<h2>{this.state.hero_name.toUpperCase()} Details</h2>
+						<div><span>id: </span>{this.state.hero_id}</div>
 						<div>
 						  <label>name:
-						    <input placeholder="name" onChange={this.handleChange} />
+						    <input placeholder="name" value={this.state.hero_update_name} onChange={this.handleChange} />
 						  </label>
 						</div>
 						<div>
-							<button id='button_save' onClick='save()'>save</button>
+							<button id='button_save' onClick={this.save}>save</button>
 							<button id='button_goBack' onClick={this.goBack}>go back</button>
 						</div>
 					</Fragment>
