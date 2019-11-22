c7c654effe2ab74ff275ba4232b7fe3daf0dc578
Jiean Yang <jiean.yang8534@gmail.com>
2019-11-22 01:21:57 +0100

    AddHero function

--- react/src/1_pages/Heroes/Heroes.js
+++ react/src/1_pages/Heroes/Heroes.js
@@ -6,7 +6,10 @@ import { Lists } from '../../3_elements/Lists'
 class Heroes extends React.Component  {
 	constructor(props) {
 		super(props)
-		this.state = {lists: []}
+		this.state = {lists: [], newHeroName: ""}
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
 	componentDidMount() {
@@ -19,6 +22,39 @@ class Heroes extends React.Component  {
 			})
 	}
 
+	handleChange(event) {
+		this.setState({newHeroName: event.target.value});
+	}
+
+	handleSubmit(event) {
+		// alert(this.state.newHeroName);
+		
+		const postHero = (url, data) => {
+		  // Default options are marked with *
+		  return fetch(url, {
+		    headers: {
+		      // 'user-agent': 'Mozilla/4.0 MDN Example',
+		      'content-type': 'application/json'
+		    },
+		    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+		    body: JSON.stringify(data), // must match 'Content-Type' header
+		    // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+		    // credentials: 'same-origin', // include, same-origin, *omit
+		    // mode: 'cors', // no-cors, cors, *same-origin
+		    // redirect: 'follow', // manual, *follow, error
+		    // referrer: 'no-referrer', // *client, no-referrer
+		  })
+		  .then(response => response.json()) // parses response to JSON
+		}
+
+		postHero("http://35.180.32.33/nodejs/heroes", {name: this.state.newHeroName})
+			.then(data => {
+				const heroes = this.state.lists;
+				heroes.push(data);
+				this.setState({lists: heroes});
+			})
+			.catch(error => console.log(error))
+	}
 
 	render() {
 		return (
@@ -30,9 +66,9 @@ class Heroes extends React.Component  {
 					<h2>My Heroes</h2>
 					<div>
 						<label>Hero name:
-							<input type="text" />
+							<input type="text" value={this.state.newHeroName} onChange={this.handleChange} />
 						</label>
-						<button>
+						<button onClick={this.handleSubmit}>
 							add
 						</button>
 					</div>
