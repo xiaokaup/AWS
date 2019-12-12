d3187638f4fa64b9fdf854f2b888c2cdaa8f544b
Jiean Yang <jiean.yang8534@gmail.com>
2019-12-12 02:38:54 +0100

    Update css of NavBar

--- react/src/2_components/Common/NavBar.js
+++ react/src/2_components/Common/NavBar.js
@@ -1,13 +1,32 @@
 import React from 'react'
+import { Container, Row, Col } from 'reactstrap'
 import './NavBar.css'
 
 const NavBar = () => (
-	<nav>
-		<a href="/">home</a>
-		<a href="/reduxApp">ReduxApp</a>
-		<a href="/Dashboard">Dashboard</a>
-		<a href="/heroes">Heroes</a>
-	</nav>
+  <div className="header">
+    <Container className="header-desktop">
+      <Row>
+        <Col sm={2}>
+          <div className="logo">
+            <a href="/"><img src="https://yjalifebook.files.wordpress.com/2019/12/cropped-e5ae87e888aae59198-2.jpeg" alt="logo"/></a>
+          </div>
+        </Col>
+        <Col sm={6}>
+          <Row className="nav">
+            <Col><a href="/">home</a></Col>
+            <Col><a href="/reduxApp">ReduxApp</a></Col>
+            <Col><a href="/Dashboard">Dashboard</a></Col>
+            <Col><a href="/heroes">Heroes</a></Col>
+          </Row>
+        </Col>
+        <Col sm={4}>
+          <div className="header-right">
+            test
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  </div>
 )
 
 export default NavBar
