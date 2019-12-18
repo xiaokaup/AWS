882cabc48ad0a30522e0af9128eeada69f26298c
Jiean Yang <jiean.yang8534@gmail.com>
2019-12-18 18:26:32 +0100

    Add button mobile

--- react/src/2_components/Common/NavBar.js
+++ react/src/2_components/Common/NavBar.js
@@ -1,45 +1,54 @@
-import React from 'react'
-import { Row, Col } from 'reactstrap'
+import React, { useState } from 'react'
+import { Row, Col, Collapse } from 'reactstrap'
 import './NavBar.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee, faAddressCard, faAt } from '@fortawesome/free-solid-svg-icons'
 
-const NavBar = () => (
-  <Row className="navBar navBar-desktop">
-    <Col sm={12} md={1}>
-      <div className="logo">
-        <a href="/"><img src="https://yjalifebook.files.wordpress.com/2019/12/cropped-e5ae87e888aae59198-2.jpeg" alt="logo"/></a>
-      </div>
-    </Col>
-    <Col sm={12} md={7}>
-      <div id="block_mobile_menu">
-        <span><div className="menu-icon"></div></span>
-      </div>
-      <Row className="navs">
-        <Col sm={6} md={3}><a href="/">home</a></Col>
-        <Col sm={6} md={3}><a href="/reduxApp">ReduxApp</a></Col>
-        <Col sm={6} md={3}><a href="/Dashboard">Dashboard</a></Col>
-        <Col sm={6} md={3}><a href="/heroes">Heroes</a></Col>
-      </Row>
-    </Col>
-    <Col sm={12} md={4}>
-      <Row className="navBar-right">
-        <Col id="block_faAddressCard">
-          <FontAwesomeIcon icon={faAddressCard} color="green" />
-          <div id="popup_faAddressCard">
-            <p>Name: <span>Jiean Yang</span></p>
-            <p>Nationality: <span>chinese</span></p>
-            <p>School: <span>ESIGELEC</span></p>
-            <p>Diploma: <span>Finance and IT engineer</span></p>
-            <p>Job: <span>Full stack developer</span></p>
-            <p>Address: <span>17 rue Antoine Courbarien</span></p>
-          </div>
-        </Col>
-        <Col><FontAwesomeIcon icon={faCoffee} color="green" /></Col>
-        <Col><FontAwesomeIcon icon={faAt} color="green" /></Col>
-      </Row>
-    </Col>
-  </Row>
-)
+
+
+const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <Row className="navBar navBar-desktop">
+      <Col sm={12} md={1}>
+        <div className="logo">
+          <a href="/"><img src="https://yjalifebook.files.wordpress.com/2019/12/cropped-e5ae87e888aae59198-2.jpeg" alt="logo"/></a>
+        </div>
+        <div id="block_mobile_menu">
+          <span className="menu-icon" onClick={() => setIsOpen(!isOpen)}></span>
+        </div>
+      </Col>
+      <Col sm={12} md={7}>
+        <Collapse isOpen={isOpen}>
+          <Row className="navs">
+            <Col sm={6} md={3}><a href="/">home</a></Col>
+            <Col sm={6} md={3}><a href="/reduxApp">ReduxApp</a></Col>
+            <Col sm={6} md={3}><a href="/Dashboard">Dashboard</a></Col>
+            <Col sm={6} md={3}><a href="/heroes">Heroes</a></Col>
+          </Row>
+        </Collapse>
+        
+      </Col>
+      <Col sm={12} md={4}>
+        <Row className="navBar-right">
+          <Col id="block_faAddressCard">
+            <FontAwesomeIcon icon={faAddressCard} color="green" />
+            <div id="popup_faAddressCard">
+              <p>Name: <span>Jiean Yang</span></p>
+              <p>Nationality: <span>chinese</span></p>
+              <p>School: <span>ESIGELEC</span></p>
+              <p>Diploma: <span>Finance and IT engineer</span></p>
+              <p>Job: <span>Full stack developer</span></p>
+              <p>Address: <span>17 rue Antoine Courbarien</span></p>
+            </div>
+          </Col>
+          <Col><FontAwesomeIcon icon={faCoffee} color="green" /></Col>
+          <Col><FontAwesomeIcon icon={faAt} color="green" /></Col>
+        </Row>
+      </Col>
+    </Row>
+  )
+}
 
 export default NavBar
