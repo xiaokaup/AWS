49f3d59509fc439250e3c8c2fdba24f9b13a0033
Jiean Yang <jiean.yang8534@gmail.com>
2019-12-26 03:20:24 +0100

    test setting of webpack (not good)

---
+++ react/config/webpack.base.conf.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.[hash].js',
+        path: path.join(__dirname, '/dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }, {
+              test: /\.js$/,
+              use: 'babel-loader',
+              exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './src/index.html'
+        }),
+        new CleanWebpackPlugin()
+    ]
+};
