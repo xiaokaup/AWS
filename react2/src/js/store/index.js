c3f422a78896d5082c0d2d4d2b95b8e07fb4dafe
Jiean Yang <jiean.yang8534@gmail.com>
2020-06-18 22:00:30 +0200

    Redux middleware

--- react2/src/js/store/index.js
+++ react2/src/js/store/index.js
@@ -1,8 +1,12 @@
 // src/js/store/index.js
 
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers/index";
+import { forbiddenWordsMiddleware } from "../middleware";
 
-const store = createStore(rootReducer);
+const store = createStore(
+	rootReducer,
+	applyMiddleware(forbiddenWordsMiddleware)
+);
 
 export default store;
