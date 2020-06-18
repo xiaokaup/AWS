d5148ce9d755b9d3c5379457aa4131d87aa2441e
Jiean Yang <jiean.yang8534@gmail.com>
2020-06-18 22:22:47 +0200

    Asynchronous actions with redux-thunk

--- react2/src/js/actions/index.js
+++ react2/src/js/actions/index.js
@@ -1,7 +1,17 @@
 // src/js/actions/index.js
 
-import { ADD_ARTICLE } from "../constants/action-types";
+import { ADD_ARTICLE, DATA_LOADED } from "../constants/action-types";
 
 export function addArticle(payload) {
   return { type: ADD_ARTICLE, payload }
 };
+
+export function getData() {
+	return function(dispatch) {
+		fetch("https://jsonplaceholder.typicode.com/posts")
+		    .then(response => response.json())
+		    .then(json => {
+		      dispatch({ type: DATA_LOADED, payload: json });
+		    });
+	}
+}
--- react2/src/js/reducers/index.js
+++ react2/src/js/reducers/index.js
@@ -1,9 +1,10 @@
 // src/js/reducers/index.js
 
-import { ADD_ARTICLE } from "../constants/action-types";
+import { ADD_ARTICLE, DATA_LOADED } from "../constants/action-types";
 
 const initialState = {
-  articles: []
+  articles: [],
+  remoteArticles: []
 };
 
 function rootReducer(state = initialState, action) {
@@ -11,6 +12,12 @@ function rootReducer(state = initialState, action) {
     return Object.assign({}, state, {
       articles: state.articles.concat(action.payload)
     });
+  }
+
+  if(action.type == DATA_LOADED) {
+  	return Object.assign({}, state, {
+  		remoteArticles: state.remoteArticles.concat(action.payload)
+  	});
   }
   return state;
 }
