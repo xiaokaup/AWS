d0e37890ce46166811ee73ab80eea4bf75b76b22
Jiean Yang <jiean.yang8534@gmail.com>
2020-06-19 19:09:22 +0200

    Add redux-logger

--- react2/src/js/store/index.js
+++ react2/src/js/store/index.js
@@ -4,7 +4,8 @@ import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers/index";
 import { forbiddenWordsMiddleware } from "../middleware";
 import createSagaMiddleware from "redux-saga";
-import apiSaga from "../sagas/api-sage"
+import apiSaga from "../sagas/api-sage";
+import { createLogger } from 'redux-logger';
 
 const initialiseSagaMiddleware = createSagaMiddleware();
 
@@ -13,7 +14,7 @@ const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
   storeEnhancers(
-    applyMiddleware(forbiddenWordsMiddleware, initialiseSagaMiddleware)
+    applyMiddleware(createLogger(), forbiddenWordsMiddleware, initialiseSagaMiddleware)
   )
 );
 
