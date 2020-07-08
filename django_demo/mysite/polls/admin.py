788ba17612a55a9533dbce94f133416a5f3dc6a3
Jiean Yang <jiean.yang8534@gmail.com>
2020-07-08 20:02:05 +0200

    Use list_display to show columns and set attribute of column

--- django_demo/mysite/polls/admin.py
+++ django_demo/mysite/polls/admin.py
@@ -11,8 +11,10 @@ class QuestionAdmin(admin.ModelAdmin):
 		(None, {'fields': ['question_text']}),
 		('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
 	]
-	inlines = [ChoiceInline]
 	
+	inlines = [ChoiceInline]
+
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
 
 # Register your models here.
 admin.site.register(Question, QuestionAdmin)
