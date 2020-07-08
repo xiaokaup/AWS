4af0874dc01e67a4904c5acaa956922402952282
Jiean Yang <jiean.yang8534@gmail.com>
2020-07-08 20:08:32 +0200

    Use search_fields

--- django_demo/mysite/polls/admin.py
+++ django_demo/mysite/polls/admin.py
@@ -18,6 +18,8 @@ class QuestionAdmin(admin.ModelAdmin):
 
 	list_filter = ['pub_date']
 
+	search_fields = ['question_text']
+
 # Register your models here.
 admin.site.register(Question, QuestionAdmin)
 
